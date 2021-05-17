e22377cba3718d9414ae45c6885612fd970b6157
Ihor Chekhunov <Ihor Chekhunov.scdesignk@gmail.com>
2021-05-17 15:15:16 +0300

    added section review and fixed section quiz

---
+++ .history/app/js/main_20210517143855.js
@@ -0,0 +1,121 @@
+$(function () {
+  $('.header__popup-btn').on('click', function (e) {
+    e.preventDefault();
+    $('body').toggleClass('body--active');
+  });
+
+  function getTimeRemaining(endtime) {
+    const total = new Date(endtime) - Date.parse(new Date());
+
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    const clock = document.querySelector('.proposition__clock');
+    const daysSpan = clock.querySelector('.proposition__days');
+    const hoursSpan = clock.querySelector('.proposition__hours');
+    const minutesSpan = clock.querySelector('.proposition__minutes');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const deadline = $('.proposition__clock').attr('data-time');
+  initializeClock('proposition__clock', deadline);
+
+  const progressSpan = document.querySelector('.quiz__line');
+
+  function changeNum(value) {
+    let className = $('.quiz__line').attr('class');
+    $('.quiz__line').text(value);
+    return node;
+  }
+
+  function changeWidth(value) {
+    let nodeWidth = (document.querySelector('.quiz__progress').style.width = value);
+    return nodeWidth;
+  }
+
+  $('.quiz__link--next').on('click', function () {
+    var $selectedQuestions = $('.quiz__questions')
+      .children('.quiz__subject--active')
+      .removeClass('quiz__subject--active');
+    if ($selectedQuestions.length == 0 || $selectedQuestions.next().length == 0) {
+      $('.quiz__questions').children('quiz__subject:eq(0)').addClass('quiz__subject--active');
+    } else {
+      $selectedQuestions.next().addClass('quiz__subject--active');
+    }
+
+    var $selectedOptions = $('.quiz__items')
+      .children('.quiz__test--active')
+      .removeClass('quiz__test--active');
+    if ($selectedOptions.length == 0 || $selectedOptions.next().length == 0) {
+      $('.quiz__items').children('quiz__test:eq(0)').addClass('quiz__test--active');
+    } else {
+      $selectedOptions.next().addClass('quiz__test--active');
+      let at = $('.quiz__test--active').attr('value-progress');
+      //сброс до дефолта чекбокс
+      // $(':radio').each(function (i, item) {
+      //   this.checked = item.defaultChecked;
+      // });
+      //заполнение шкалы процента и число процента
+      changeWidth(at + '%');
+      changeNum(at);
+    }
+  });
+
+  // Закрытие по клавише Esc.
+  // $(document).on('keydown', function (e) {
+  //   if (e.keyCode === 27) {
+  //     e.stopPropagation();
+  //     $('.review-card').fadeOut();
+  //   }
+  // });
+
+  // Клик по фону, но не по окну.
+  $('.review-card').on('click', function (e) {
+    if ($(e.target).closest('.review-card__popup--active').length == 0) {
+      $(this).fadeOut();
+    }
+  });
+
+  // добавление в массив значения чекбокса value
+  var checklist = {};
+
+  $(document).on('change', '.quiz__input-checkbox', function () {
+    var val = this.value | 0; // to int
+
+    if (this.checked) {
+      checklist.push(val); // если в начало, то .ushift(val)
+    } else {
+      var idx = $.inArray(val, checklist);
+      if (idx > -1) {
+        checklist.splice(idx, 1);
+      }
+    }
+
+    $('');
+
+    console.log(checklist);
+  });
+});
